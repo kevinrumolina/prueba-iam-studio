c32f48d3e1efca22569775a82144ef61c9548866
Kevin Ruiz Molina <kevinrumolina26@gmail.com>
2021-06-09 00:18:10 -0500

    Test B solution

--- main.js
+++ main.js
@@ -8,4 +8,31 @@ const firstTenNumbers = multiple => {
     return resultArray;
 };
 
+/*const gridConstructor = (rows, columns) => {
+    const gridContainer = document.createElement('section');
+    for (let row = 1; row <= rows; row++) {
+        let rowComponent = Object.assign(document.createElement('div'), { className:`fila fila-${row}` });
+
+        for (let column = 1; column <= columns; column++) {
+            let columnComponent = Object.assign(document.createElement('div'), { className: `item item-${column}` });
+
+            rowComponent.appendChild(columnComponent);
+        }
+
+        gridContainer.appendChild(rowComponent);
+    }
+
+    document.querySelector('body').appendChild(gridContainer);
+}*/
+
+const matrixFiller = (rows, columns) => {
+    for (let row = 1; row <= rows; row++) {
+        let rowItems = document.querySelectorAll(`.test-b .fila-${row} .item`);
+        for (let column = 1; column <= columns; column++) {
+            rowItems[column - 1].innerText = `${row},${column}`;
+        }
+    }
+}
+
 console.log(firstTenNumbers(2));
+matrixFiller(5,5);
